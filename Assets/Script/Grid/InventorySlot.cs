589206571033bb5d5ce07b5a8be26b6026fdacaa
Max Le <minhphuongsony@gmail.com>
2023-11-27 12:47:59 +1100

    second commit

--- Assets/Script/Grid/InventorySlot.cs
+++ Assets/Script/Grid/InventorySlot.cs
@@ -4,4 +4,13 @@ using UnityEngine.UI;
 public class InventorySlot : MonoBehaviour
 {
     public Node node;
+
+
+    private void Start()
+    {
+        if (node.isAvailable)
+        {
+
+        }
+    }
 }
